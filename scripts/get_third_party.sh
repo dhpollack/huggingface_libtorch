8099c9cd0106f39549072f6e6f5487b5b379bf54
David Pollack <david@da3.net>
2020-01-14 15:15:39 +0100

    download nlohmann

--- scripts/get_third_party.sh
+++ scripts/get_third_party.sh
@@ -4,9 +4,11 @@ cd $(dirname $0)/..
 
 mkdir -p third_party && cd third_party
 
+THIRDPARTYLOCAL="local"
 SENTENCEPIECEURI="https://github.com/google/sentencepiece.git"
 LIBTORCHURI="https://download.pytorch.org/libtorch/cpu/libtorch-shared-with-deps-1.3.1%2Bcpu.zip"
 BOOSTURI="https://dl.bintray.com/boostorg/release/1.72.0/source/boost_1_72_0.tar.gz"
+NLOHMANNURI="https://github.com/nlohmann/json.git"
 
 echo "Installing sentencepiece, make sure you've installed the requirements, which on Ubuntu is:"
 echo "sudo apt-get install cmake build-essential pkg-config libgoogle-perftools-dev"
@@ -14,7 +16,7 @@ echo "sudo apt-get install cmake build-essential pkg-config libgoogle-perftools-
 git clone $SENTENCEPIECEURI
 cd sentencepiece
 mkdir -p build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=$(realpath ../../sp) ..
+cmake -DCMAKE_INSTALL_PREFIX=$(realpath ../../${THIRDPARTYLOCAL}) ..
 make -j $(nproc --ignore=1)
 make install
 # cleaning up
@@ -27,8 +29,17 @@ wget $LIBTORCHURI -O libtorch.zip
 unzip libtorch.zip
 rm libtorch.zip
 
-echo "installing boost"
-wget $BOOSTURI -O boost.tar.gz
-tar xzf boost.tar.gz
-rm boost.tar.gz
+echo "installing nlohmann json..."
+git clone ${NLOHMANNURI}
+cd json
+mkdir build && cd build
+cmake -DJSON_BuildTests=OFF -DCMAKE_INSTALL_PREFIX=$(realpath ../../${THIRDPARTYLOCAL}) ..
+make install
+cd ..
+
+
+#echo "installing boost"
+#wget $BOOSTURI -O boost.tar.gz
+#tar xzf boost.tar.gz
+#rm boost.tar.gz
 
