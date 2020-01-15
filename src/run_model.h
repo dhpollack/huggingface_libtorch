7f147582e8025169f6cf93ec6d4b54daafa8d64d
David Pollack <david@da3.net>
2020-01-15 14:22:42 +0100

    forgot to add the new run_model.h file with last commit

---
+++ src/run_model.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <torch/script.h>
+#include <torch/torch.h>
+
+#include <fstream>
+#include <iostream>
+#include <ostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+
